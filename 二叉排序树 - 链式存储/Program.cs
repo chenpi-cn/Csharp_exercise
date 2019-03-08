bb75b5cdaee21d3926abc955b9791a7d51b14289
Huangyingran <52tech.fun@gmail.com>
2019-03-08 16:03:04 +0800

    C#练习

---
+++ 二叉排序树 - 链式存储/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 二叉排序树___链式存储
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BSTree tree = new BSTree();
+            int[] data = { 62, 58, 88, 47, 73, 99, 35, 51, 93, 37 };
+            foreach (int t in data)
+            {
+                tree.Add(t);
+            }
+            tree.MiddleTraversal();
+            Console.WriteLine();
+            Console.WriteLine(tree.Find(99));
+            Console.WriteLine(tree.Find(100));
+            tree.Delete(35);
+            tree.MiddleTraversal(); Console.WriteLine();
+            tree.Delete(62);
+            tree.MiddleTraversal(); Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
